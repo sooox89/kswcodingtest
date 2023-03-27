50281f414c8cd16bed68e7e656e252c4dc96a65b
sooox0809 <skycasey@inha.edu>
2023-03-28 01:10:00 +0900

    week1 주사위 게임 문제 #2476

--- week1.py
+++ week1.py
@@ -1,19 +1,19 @@
-# 백준 1267번
-# 영식 Y : 30초 마다 10원씩 29초까지 10원 , 30초부터 59초 : 10원 추가
-# 민식 M : 60초 마다 15원씩 59초까지 15원 , 60초부터 119초 : 15원 추가
-
+# 백준 2476번 주사위 게임
 from sys import stdin
-N = int(input())
-sec = list(map(int, stdin.readline().split()))
-y = 0
-m = 0
-for i in sec:
-    y += i//30 * 10 + 10
-    m += i//60 * 15 + 15
-if y > m:
-    print('M',m)
-elif y < m:
-    print('Y',y)
-else:
-    print('Y M',y)
+N = int(stdin.readline().strip()) ## int(input())
+winning = 0
+winner = 0
+for i in range(N):
+    a, b, c = map(int, stdin.readline().split())
+    if a == b == c:
+        winning = 10000 + a * 1000
+    elif a == b or a == c:
+        winning = 1000 + a * 100
+    elif b == c:
+        winning = 1000 + b * 100
+    else:
+        winning = max(a, b, c) * 100
 
+    if winning > winner:
+        winner = winning
+print(winner)
