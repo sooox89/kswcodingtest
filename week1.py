ff641298a10fe933ceb9b1f61a5cd39cdfd198a7
sooox0809 <skycasey@inha.edu>
2023-03-28 00:00:50 +0900

    week1 휴대폰 요금 문제

--- week1.py
+++ week1.py
@@ -1,17 +1,19 @@
-# 100 80 70 60
-# 80 70 80 90
+# 백준 1267번
+# 영식 Y : 30초 마다 10원씩 29초까지 10원 , 30초부터 59초 : 10원 추가
+# 민식 M : 60초 마다 15원씩 59초까지 15원 , 60초부터 119초 : 15원 추가
 
 from sys import stdin
-
-MG_info, MG_math, MG_science, MG_eng = map(int, stdin.readline().split())
-# print(f'input: {MG_info, MG_math, MG_science, MG_eng}')
-MS_info, MS_math, MS_science, MS_eng = map(int, stdin.readline().split())
-# print(f'input: {MS_info, MS_math, MS_science, MS_eng}')
-
-S = MG_info + MG_math + MG_science + MG_eng
-T = MS_info + MS_math + MS_science + MS_eng
-
-if S < T:
-    print(T)
+N = int(input())
+sec = list(map(int, stdin.readline().split()))
+y = 0
+m = 0
+for i in sec:
+    y += i//30 * 10 + 10
+    m += i//60 * 15 + 15
+if y > m:
+    print('M',m)
+elif y < m:
+    print('Y',y)
 else:
-    print(S)
+    print('Y M',y)
+
