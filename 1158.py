efce4632ca66c2c0a2e27354094e383ede08d44b
sooox0809 <skycasey@inha.edu>
2023-04-01 17:16:18 +0900

    week2 1158번 요세푸스

--- 1158.py
+++ 1158.py
@@ -4,14 +4,13 @@ from sys import stdin
 n, k = map(int, stdin.readline().split())
 queue = [i+1 for i in range(n)]
 answer = []
+front = 0
+rear = 0
 
 while len(queue) !=0:
-    for i in range(k-1):
-        data = queue[0]
-        queue.pop(0)
-        queue.append(data)
-    data = queue[0]
-    queue.pop(0)
+    data = queue[(front+k-1)%len(queue)]
+    front = (front + k-1) % len(queue)
+    queue.pop(front)
     answer.append(data)
 print('<', end='')
 print(*answer, sep = ', ' , end = '')
