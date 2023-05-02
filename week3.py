bb16bb1a44786180dee6840a4186e52e8adf9a71
sooox0809 <skycasey@inha.edu>
2023-05-02 16:10:57 +0900

    week3 tree list

--- week3.py
+++ week3.py
@@ -1,20 +1,11 @@
-# Tree_matrix
+# Tree_list
+
 n = 5
+graph = [[] for _ in range(n+1)]
 
-# 6x6 행렬 만들기 -> 각 0번째 idx 사용을 안할 것임
-matrix =[[0]*(n+1) for _ in range(n+1)]
-
-matrix[1][2]=1
-matrix[2][1]=1
-
-
-matrix[1][3]=1
-matrix[3][1]=1
-
-matrix[3][4]=1
-matrix[4][3]=1
-
-matrix[3][5]=1
-matrix[5][3]=1
-
-print(matrix)
+graph[1] = [2,3]
+graph[2] =[]
+graph[3] = [4,5]
+graph[4] = []
+graph[5] = []
+print(graph)
