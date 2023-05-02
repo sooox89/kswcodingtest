a90791777c1ab854c144be44fdb2ae06acff04f8
sooox0809 <skycasey@inha.edu>
2023-05-02 15:13:23 +0900

    week3 tree matrix

---
+++ week3.py
@@ -0,0 +1,20 @@
+# Tree_matrix
+n = 5
+
+# 6x6 행렬 만들기 -> 각 0번째 idx 사용을 안할 것임
+matrix =[[0]*(n+1) for _ in range(n+1)]
+
+matrix[1][2]=1
+matrix[2][1]=1
+
+
+matrix[1][3]=1
+matrix[3][1]=1
+
+matrix[3][4]=1
+matrix[4][3]=1
+
+matrix[3][5]=1
+matrix[5][3]=1
+
+print(matrix)
